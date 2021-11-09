0a770892aa0e547ff9e7e6a3a4d25de2fbc9a60b
Serra <pacoserranox@gmail.com>
2021-11-09 19:53:22 +0100

    commit

--- AccesoDatos/Program.cs
+++ AccesoDatos/Program.cs
@@ -7,7 +7,49 @@ namespace AccesoDatos
     {
         static void Main(string[] args)
         {
+            AccionRegistros();
+            EliminarDepartamento();
             LeerRegistros();
+        }
+
+        static void EliminarDepartamento()
+        {
+            String cadenaconexion = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=azure";
+            SqlConnection cn = new SqlConnection(cadenaconexion);
+            SqlCommand com = new SqlCommand();
+            String sql = "DELETE FROM DEPT WHERE DEPT_NO=@NUMERO";
+            Console.WriteLine("Número departamento a eliminar: ");
+            int numero = int.Parse(Console.ReadLine());
+            SqlParameter pamnumero = new SqlParameter("@NUMERO", numero);
+            com.Connection = cn;
+            com.CommandText = sql;
+            com.CommandType = System.Data.CommandType.Text;
+            //INCLUIMOS LOS PARAMETROS DENTRO DEL COMANDO
+            com.Parameters.Add(pamnumero);
+            cn.Open();
+            int eliminados = com.ExecuteNonQuery();
+            Console.WriteLine("Departamentos eliminados: " + eliminados);
+            //NO IMPORTA EL ORDEN...
+            cn.Close();
+            //LIBERAMOS LOS PARAMETROS DEL COMANDO
+            com.Parameters.Clear();
+        }
+
+        static void AccionRegistros()
+        {
+            String cadenaconexion = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=azure";
+            SqlConnection cn = new SqlConnection(cadenaconexion);
+            SqlCommand com = new SqlCommand();
+            String sql = "INSERT INTO DEPT VALUES (66, 'INFORMATICA', 'GIJON')";
+            com.Connection = cn;
+            com.CommandText = sql;
+            com.CommandType = System.Data.CommandType.Text;
+            cn.Open();
+            //LAS CONSULTAS DE ACCION SE EJECUTAR CON ExecuteNonQuery()
+            //Y DEVUELVEN EL NUMERO DE REGISTROS QUE HAN SIDO AFECTADOS
+            int insertados = com.ExecuteNonQuery();
+            cn.Close();
+            Console.WriteLine("Insertados: " + insertados);
         }
 
         static void LeerRegistros()
